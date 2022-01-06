49c4960bce3daef99e30c5de3682b9feab52b556
eitan-rosati <eitanrosati@gmail.com>
2022-01-06 11:51:42 +0200

    add extraction of the authors

--- Project/data_handler.py
+++ Project/data_handler.py
@@ -5,11 +5,12 @@ class data_processor:
     def extract_data(self):
         import pandas as pd
         # Extract the fields we need from the DB
-        ids, records_isbn, titles, topic_by_record, descriptions = self.extract_seeds()
+        ids, records_isbn, titles, topic_by_record, descriptions, authors = self.extract_seeds()
         # normalize the topics to list of phrases.
         normalized_topics = self.normalize_topics(topic_by_record)
         book_dataset = pd.DataFrame({"id": ids,
                                      "title": titles,
+                                     "author": authors,
                                      "topics": normalized_topics,
                                      "description": descriptions,
                                      "isbn": records_isbn})
@@ -18,12 +19,13 @@ class data_processor:
     def extract_seeds(self):
         import xml.etree.ElementTree as ET
         """
-        Extracting from the MARC file the following information:
+        Extracting from the MARC file the following informations: 
             001 - book mms id
             020 - book isbn
             245 - book title
             520 - book description
             650 - book topics
+            100 - book author
         """
         xml_tree = ET.parse(self.file_name)
         xml_root = xml_tree.getroot()
@@ -34,18 +36,20 @@ class data_processor:
         descriptions = []
         records_isbn = []
         topic_by_record = []
-
-        # local variables to extract the data from file
-        description = ''
-        isbn = ''
-        title = ''
-        mms_id = ''
+        authors = []
         for record in xml_root:
+            # local varaiables to extract the data from file
+            description = None
+            isbn = None
+            title = None
+            mms_id = None
+            author = None
+            record_topics = []
+            # check each of the subfield and extract the information
             for field in record.findall('controlfield'):
                 if field.get('tag') == '001':
                     mms_id = field.text
                     break
-            record_topics = []
             for field in record.findall('datafield'):
                 if not enter:
                     enter = True
@@ -59,23 +63,26 @@ class data_processor:
                             title = subfield.text
                             if not title[-1].isalpha():
                                 title = title[:-1]
-                elif field.get('tag') == '020' and isbn == "":
+                elif field.get('tag') == '020':
                     for subfield in field.findall('subfield'):
                         if subfield.get('code') == 'a':
-                            isbn = subfield.text.split(" ")[0]
+                            isbn = subfield.text
                 elif field.get('tag') == '520':
                     for subfield in field.findall('subfield'):
                         if subfield.get('code') == 'a':
                             description = subfield.text
-
-            # add the information we extract to our lists
+                elif field.get('tag') == '100':
+                    for subfield in field.findall('subfield'):
+                        if subfield.get('code') == 'a':
+                            author = subfield.text
             topic_by_record.append(record_topics)
             titles.append(title)
             ids.append(mms_id)
             descriptions.append(description)
             records_isbn.append(isbn)
+            authors.append(author)
 
-        return ids, records_isbn, titles, topic_by_record, descriptions
+        return ids, records_isbn, titles, topic_by_record, descriptions, authors
 
     def normalize_topics(self, topic_by_record):
         normalized_topics = []
