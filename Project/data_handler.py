8c4c2c4218d87ce077b9b8996decefa2ff273fa5
eitan-rosati <eitanrosati@gmail.com>
2022-01-06 13:50:54 +0200

    Update data_handler.py

--- Project/data_handler.py
+++ Project/data_handler.py
@@ -1,33 +1,39 @@
-"""Eitan Rosati, Software Engineering Department - Azrieli College of Engineering"""
+"""
+Eitan Rosati, Software Engineering Department - Azrieli College of Engineering
+This class extract the information we got from the database and normalize the topics
+"""
 
 
 class data_processor:
     def extract_data(self):
         import pandas as pd
         # Extract the fields we need from the DB
-        ids, records_isbn, titles, topic_by_record, descriptions, authors = self.extract_seeds()
+        ids, records_isbn, titles, topic_by_record, descriptions, authors, book_pages = self.extract_seeds()
         # normalize the topics to list of phrases.
         normalized_topics = self.normalize_topics(topic_by_record)
         book_dataset = pd.DataFrame({"id": ids,
                                      "title": titles,
                                      "author": authors,
+                                     "pages": book_pages,
                                      "topics": normalized_topics,
                                      "description": descriptions,
                                      "isbn": records_isbn})
         return book_dataset
 
     def extract_seeds(self):
-        import xml.etree.ElementTree as ET
         """
-        Extracting from the MARC file the following informations: 
+        Extracting from the MARC file the following informations:
             001 - book mms id
             020 - book isbn
             245 - book title
             520 - book description
             650 - book topics
             100 - book author
+            300 - book number of pages
         """
-        xml_tree = ET.parse(self.file_name)
+        import xml.etree.ElementTree as ET
+        import re
+        xml_tree = ET.parse("BIBLIOGRAPHIC_64846678830003941_1.xml")
         xml_root = xml_tree.getroot()
         enter = False
         # lists for the books information
@@ -37,14 +43,16 @@ class data_processor:
         records_isbn = []
         topic_by_record = []
         authors = []
+        book_pages = []
         for record in xml_root:
-            # local varaiables to extract the data from file
+            # local variables to extract the data from file
             description = None
             isbn = None
             title = None
             mms_id = None
             author = None
             record_topics = []
+            pages = None
             # check each of the subfield and extract the information
             for field in record.findall('controlfield'):
                 if field.get('tag') == '001':
@@ -75,14 +83,23 @@ class data_processor:
                     for subfield in field.findall('subfield'):
                         if subfield.get('code') == 'a':
                             author = subfield.text
+                elif field.get('tag') == '300':
+                    for subfield in field.findall('subfield'):
+                        if subfield.get('code') == 'a':
+                            b_pages = re.sub("[^0-9]", "", subfield.text)
+                            if b_pages != "":
+                                pages = int(b_pages)
+                            else:
+                                pages = None
             topic_by_record.append(record_topics)
             titles.append(title)
             ids.append(mms_id)
             descriptions.append(description)
             records_isbn.append(isbn)
             authors.append(author)
+            book_pages.append(pages)
 
-        return ids, records_isbn, titles, topic_by_record, descriptions, authors
+        return ids, records_isbn, titles, topic_by_record, descriptions, authors, book_pages
 
     def normalize_topics(self, topic_by_record):
         normalized_topics = []
